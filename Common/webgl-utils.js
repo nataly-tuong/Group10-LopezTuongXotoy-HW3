0fdf620dd86da5f4d39bdbf8bbd314c252033bd1
nataly-tuong <152276169+nataly-tuong@users.noreply.github.com>
2026-09-14 22:14:28 -0700

    replaced common files with the ones from the last homework

--- Common/webgl-utils.js
+++ Common/webgl-utils.js
@@ -1,82 +1,67 @@
-(function () {
-  "use strict";
+var WebGLUtils = function()
+{
+    function setupWebGL(canvas, opt_attribs, opt_onError)
+    {
+        var names = ["webgl2"];
 
-  window.getShaderSource = function (id) {
-    return document.getElementById(id).textContent.replace(/^\s+|\s+$/g, "");
-  };
+        var context = create3DContext(
+            canvas,
+            opt_attribs,
+            names
+        );
 
-  function createShader(gl, source, type) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    return shader;
-  }
-
-  window.createProgram = function (
-    gl,
-    vertexShaderSource,
-    fragmentShaderSource,
-  ) {
-    var program = gl.createProgram();
-    var vshader = createShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
-    var fshader = createShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER);
-    gl.attachShader(program, vshader);
-    gl.deleteShader(vshader);
-    gl.attachShader(program, fshader);
-    gl.deleteShader(fshader);
-    gl.linkProgram(program);
-
-    var log = gl.getProgramInfoLog(program);
-    if (log) {
-      console.log(log);
-    }
-
-    log = gl.getShaderInfoLog(vshader);
-    if (log) {
-      console.log(log);
-    }
-
-    log = gl.getShaderInfoLog(fshader);
-    if (log) {
-      console.log(log);
-    }
-
-    return program;
-  };
-  /*
-   window.loadImage = function(url, onload) {
-        var img = new Image();
-        img.src = url;
-        img.onload = function() {
-            onload(img);
-        };
-        return img;
-    };
-
-    window.loadImages = function(urls, onload) {
-        var imgs = [];
-        var imgsToLoad = urls.length;
-
-        function onImgLoad() {
-            if (--imgsToLoad <= 0) {
-                onload(imgs);
+        if (!context)
+        {
+            if (opt_onError)
+            {
+                opt_onError();
+            }
+            else
+            {
+                alert(
+                    "WebGL 2.0 is not available."
+                );
             }
         }
 
-        for (var i = 0; i < imgsToLoad; ++i) {
-            imgs.push(loadImage(urls[i], onImgLoad));
-        }
-    };
+        return context;
+    }
 
-    window.loadObj = function(url, onload) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'text';
-        xhr.onload = function(e) {
-            var mesh = new OBJ.Mesh(this.response);
-            onload(mesh);
-        };
-        xhr.send();
+
+    function create3DContext(
+        canvas,
+        opt_attribs,
+        opt_context_names
+    )
+    {
+        var context = null;
+
+        for (var ii = 0;
+             ii < opt_context_names.length;
+             ++ii)
+        {
+            try
+            {
+                context = canvas.getContext(
+                    opt_context_names[ii],
+                    opt_attribs
+                );
+            }
+            catch (e)
+            {
+            }
+
+            if (context)
+            {
+                break;
+            }
+        }
+
+        return context;
+    }
+
+
+    return {
+        setupWebGL: setupWebGL
     };
-*/
-})();
+}();
